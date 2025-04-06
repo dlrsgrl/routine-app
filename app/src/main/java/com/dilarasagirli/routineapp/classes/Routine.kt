97c98f9a379a6caeb69db4ed81afd35af504aa46
dlrsgrl <zdilarasgrl@gmail.com>
2025-04-06 18:00:32 +0300

    Delete app/src/main/java/com/dilarasagirli/routineapp/classes directory

--- app/src/main/java/com/dilarasagirli/routineapp/classes/Routine.kt
+++
@@ -1,6 +0,0 @@
-package com.dilarasagirli.routineapp.classes
-
-import java.io.Serializable
-
-class Routine (var name: String, var totalTime: Int?, var taskNumber:Int?, var isCompleted: Boolean,var tasks: ArrayList<Tasks>?): Serializable
-
--- app/src/main/java/com/dilarasagirli/routineapp/classes/Tasks.kt
+++
@@ -1,5 +0,0 @@
-package com.dilarasagirli.routineapp.classes
-
-import java.io.Serializable
-
-class Tasks (var name: String, var time:Int,var isCompleted:Boolean) :Serializable
